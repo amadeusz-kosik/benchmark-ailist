97a3d3951a0de65404f83feceef87ad48a3d1da8
Amadeusz Kosik <tappers_teams.5d@icloud.com>
2026-05-04 12:28:08 +0200

    Switch Array to ArrayBuilder and ArrayDeque in Scala implementation.

--- algorithms/src/main/scala/benchmark/ailist/scala/AIListBuilder.scala
+++ algorithms/src/main/scala/benchmark/ailist/scala/AIListBuilder.scala
@@ -2,6 +2,7 @@ package benchmark.ailist.scala
 
 import benchmark.{Configuration, Interval}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 
@@ -12,15 +13,12 @@ object AIListBuilder {
     assert(configuration.intervalsCountToCheckLookahead > 0)
     assert(configuration.maximumComponentSize > 0)
 
-    var intervals = sourceIntervals
-      .sorted(Ordering.by[Interval, (Long, Long)](i => (i.from, i.to)))
-      .toBuffer
-
+    var intervals = mutable.ArrayDeque.from(sourceIntervals.sorted(Ordering.by[Interval, (Long, Long)](i => (i.from, i.to))))
     val results = ArrayBuffer[AIList]()
 
     while(intervals.nonEmpty) {
-      val newComponent = ArrayBuffer[Interval]()
-      val leftovers = ArrayBuffer[Interval]()
+      val newComponent = mutable.ArrayBuilder.make[Interval]
+      val leftovers = mutable.ArrayDeque[Interval]()
 
       while(intervals.nonEmpty && newComponent.length < configuration.maximumComponentSize) {
         val nextInterval = intervals.head
@@ -44,7 +42,7 @@ object AIListBuilder {
         leftovers.addAll(intervals)
 
       intervals = leftovers
-      results += AIList(newComponent.toArray)
+      results += AIList(newComponent.result())
     }
 
     results.toArray
